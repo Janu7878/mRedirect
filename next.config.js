614071d2767a9a402fc862baad18ca22f97417a8
Janu7878 <119414805+Janu7878@users.noreply.github.com>
2022-12-28 23:21:49 +0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,53 +1,46 @@
-const indexSearch = require('./plugins/search-index');
-const feed = require('./plugins/feed');
-const sitemap = require('./plugins/sitemap');
-// const socialImages = require('./plugins/socialImages'); TODO: failing to run on Netlify
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-
-  // By default, Next.js removes the trailing slash. One reason this would be good
-  // to include is by default, the `path` property of the router for the homepage
-  // is `/` and by using that, would instantly create a redirect
-
-  trailingSlash: true,
-
-  // By enabling verbose logging, it will provide additional output details for
-  // diagnostic purposes. By default is set to false.
-  // verbose: true,
-
-  env: {
-    // The image directory for open graph images will be saved at the location above
-    // with `public` prepended. By default, images will be saved at /public/images/og
-    // and available at /images/og. If changing, make sure to update the .gitignore
-
-    OG_IMAGE_DIRECTORY: '/images/og',
-
-    // By default, only render this number of post pages ahead of time, otherwise
-    // the rest will be rendered on-demand
-    POSTS_PRERENDER_COUNT: 5,
-
-    WORDPRESS_GRAPHQL_ENDPOINT: process.env.WORDPRESS_GRAPHQL_ENDPOINT,
-    WORDPRESS_MENU_LOCATION_NAVIGATION: process.env.WORDPRESS_MENU_LOCATION_NAVIGATION || 'PRIMARY',
-    WORDPRESS_PLUGIN_SEO: parseEnvValue(process.env.WORDPRESS_PLUGIN_SEO, false),
-  },
-};
-
-module.exports = () => {
-  const plugins = [indexSearch, feed, sitemap];
-  return plugins.reduce((acc, plugin) => plugin(acc), nextConfig);
-};
+/* eslint-disable @typescript-eslint/no-var-requires */
 
 /**
- * parseEnv
- * @description Helper function to check if a variable is defined and parse booelans
+ * @type {import("next").NextConfig}
  */
+const nextConfig = {
+	reactStrictMode: true,
+	poweredByHeader: false,
+	trailingSlash: true,
+	swcMinify: true,
+	i18n: {
+		locales: ["en", "tr"],
+		defaultLocale: "en",
+	},
+	images: {
+		domains: ["avatars.githubusercontent.com", "api.338.rocks"],
+	},
+	basePath: '',
+	async redirects() {
+		return [
+			{
+				source: "/github",
+				destination: "https://github.com/barisbored",
+				permanent: true,
+			},
+			{
+				source: "/posts/:slug",
+				destination: "https://fancy4work.com/:slug",
+				basePath: false,
+				permanent: true,
+			},
+			{
+				source: "/youtube",
+				destination: "https://www.youtube.com/@barisbored",
+				permanent: true,
+			},
+			{
+				source: "/discord",
+				destination: "https://discord.gg/BjEJFwh",
+				permanent: true,
+			},
+		];
+	},
+};
 
-function parseEnvValue(value, defaultValue) {
-  if (typeof value === 'undefined') return defaultValue;
-  if (value === true || value === 'true') return true;
-  if (value === false || value === 'false') return false;
-  return value;
-}
+module.exports = nextConfig;
